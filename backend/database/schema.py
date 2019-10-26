8f37e42ef4ed998149b4c1427a546ee0328e5dad
EvertonTomalok <evertontomalok123@gmail.com>
2019-10-26 18:01:23 -0300

    Insert schema

--- backend/database/schema.py
+++ backend/database/schema.py
@@ -1,6 +1,9 @@
 from schematics.models import Model
 from schematics.types import (
     StringType,
+    IntType,
+    DateTimeType,
+    TimestampType,
 )
 
 
@@ -8,6 +11,13 @@ class Cliente(Model):
     nome = StringType(required=True)
     cpf = StringType(required=True)
     telefone = StringType(default="")
+
+
+class Comanda(Model):
+    cliente_id = IntType(required=True)
+    inicio = TimestampType(required=True)
+    fim = TimestampType(serialize_when_none=True)
+    data_comanda = DateTimeType(required=True)
 
 
 def validate_document(data, cls):
