935b2245ba4358ce315459bccae9416743197e40
Everton Tomalok <evertontomalok123@gmail.com>
2019-10-26 17:33:21 -0300

    [Clientes] Adding CRUD (#4)
    * Pipfile and first structre api
    
    * Cliente add
    
    * Delete Route
    
    * refactoring
    
    * Schemas created
    
    * edit route
    
    * find route
    
    * Added more client values
    
    * Route annotation
    
    * Renaming functions

---
+++ backend/database/dml.py
@@ -0,0 +1,50 @@
+from backend.database.db import Database
+from backend.database.schema import Cliente
+import logging
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
+
+
+class DML:
+    def __init__(self):
+        self.conn = Database().create_connection()
+
+    def destroy_me(self):
+        try:
+            self.conn.close()
+        except Exception as err:
+            logging.critical(err, type(err))
+
+    def insert_client(self, cliente: Cliente):
+        try:
+            self.conn.execute(
+                "INSERT INTO Clientes (nome, cpf, telefone) VALUES (?, ?, ?)",
+                (cliente.nome, cliente.cpf, cliente.telefone)
+            )
+            self.conn.commit()
+        except Exception as err:
+            logging.critical(type(err), err)
+
+    def delete_client(self, _id: int):
+        try:
+            self.conn.execute("DELETE FROM Clientes WHERE ID = ?", str(_id))
+            self.conn.commit()
+        except Exception as err:
+            logging.critical(type(err), err)
+
+    def edit_client(self, set_query, where):
+        query = f"UPDATE Clientes SET {set_query} WHERE {where}"
+        try:
+            self.conn.execute(query)
+            self.conn.commit()
+        except Exception as err:
+            logging.critical(type(err), err)
+
+    def find_client(self, where):
+        try:
+            execute = self.conn.execute(f"SELECT * FROM Clientes WHERE {where}")
+            fetch = execute.fetchone()
+            return {k[0]: v for k, v in list(zip(execute.description, fetch))}
+        except Exception as e:
+            logging.critical(e, type(e))
+            return {}
---
+++ backend/server.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, jsonify, abort
+from database.dml import *
+from database.schema import Cliente, validate_document
+
+
+app = Flask(__name__)
+
+
+##############################################
+#                  Clientes                  #
+##############################################
+@app.route("/client/add", methods=["POST"])
+def add_client():
+    data = request.get_json(silent=True)
+    dml = DML()
+
+    cliente = validate_document(data, Cliente)
+
+    try:
+        dml.insert_client(cliente)
+    except Exception as err:
+        logging.critical(err, type(err))
+    finally:
+        dml.destroy_me()
+
+    return jsonify(ok=200)
+
+
+@app.route("/client/del", methods=["POST"])
+def del_client():
+    data = request.get_json(silent=True)
+    dml = DML()
+    try:
+        dml.delete_client(data["id"])
+
+    except Exception as err:
+        logging.critical(err, type(err))
+    finally:
+        dml.destroy_me()
+
+    return jsonify(ok=200)
+
+
+@app.route("/client/edit", methods=["POST"])
+def edit_client():
+    data = request.get_json(silent=True)
+    dml = DML()
+
+    try:
+        dml.edit_client(data["query"], data["where"])
+    except Exception as err:
+        logging.critical(err, type(err))
+    finally:
+        dml.destroy_me()
+
+    return jsonify(ok=200)
+
+
+@app.route("/client/find", methods=["POST"])
+def find_client():
+    data = request.get_json(silent=True)
+    dml = DML()
+
+    try:
+        json_ = dml.find_client(data["where"])
+    except Exception as err:
+        logging.critical(err, type(err))
+        json_ = {}
+    finally:
+        dml.destroy_me()
+
+    return jsonify(json_)
+
+
+@app.route("/health")
+def health():
+    return "ok"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5050, threaded=True)
